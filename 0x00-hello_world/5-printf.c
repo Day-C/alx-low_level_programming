8532e26760031aa8f3f6fc15d975de21d60ab208
Day-C <kongranalanyuy@gmail.com>
2023-03-17 06:47:00 -0700

    printf-commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#insert <stdio.h>
+/**
+ * main - Enty point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
