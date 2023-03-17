2b240687606b8228330b9cf729e66f21678e1310
Day-C <kongranalanyuy@gmail.com>
2023-03-17 05:52:58 -0700

    code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - main is our main entry
+ *return: will always return a 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
