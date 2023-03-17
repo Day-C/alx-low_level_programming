22b39ce6d25008d9336c30e9f32d5803817b6f42
Day-C <kongranalanyuy@gmail.com>
2023-03-17 07:00:49 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#insert <stdio.h>
+#include <stdio.h>
 /**
  * main - Enty point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
