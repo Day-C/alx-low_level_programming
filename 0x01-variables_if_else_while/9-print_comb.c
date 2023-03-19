100155954143e875dfc3adbf39dce87604b02441
Day-C <kongranalanyuy@gmail.com>
2023-03-19 10:13:18 -0700

    cheking

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,13 +7,10 @@ int main(void)
 {
 	int i;
 
-	for (i = 0 ; i <= 9 ; i++)
+	for (i = 0 ; i < 10 ; i++);
 	{
 		if (i == 9)
-		{
-			putchar(i + '0');
 			putchar(' ');
-		}
 		else
 		{
 			putchar(i + '0');
