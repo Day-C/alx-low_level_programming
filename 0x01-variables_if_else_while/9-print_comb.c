5450ac67403518af4f43d8620c2f272f59356ed5
Day-C <kongranalanyuy@gmail.com>
2023-03-19 09:39:56 -0700

    orderly

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - ordered single digits
+ * Return: 0 is a go
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 10 ; i++)
+	{
+		if (i == 9)
+			putchar(i + '0');
+		else
+		{
+			putchar(i + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
