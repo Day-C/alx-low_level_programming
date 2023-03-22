2ef89edd329a116f283736c887d32373026051f7
Day-C <kongranalanyuy@gmail.com>
2023-03-22 16:41:56 -0700

    adding_Two-Numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - adds two numbers
+ * @f: the first parameter to the function
+ * @s: the second parameter to the function
+ * Return: returs the answe when successful
+ */
+int add(int f, int s)
+{
+	int sum;
+
+	sum = (f + s);
+	return (sum);
+}
