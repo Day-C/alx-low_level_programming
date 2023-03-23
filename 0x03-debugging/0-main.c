2e11e2f2b61b20d07a48afc2c3793326468c7b20
Day-C <kongranalanyuy@gmail.com>
2023-03-23 07:45:18 -0700

    checkFor_+or-

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,16 @@
 #include "main.h"
+
 /**
- * positive_or_negative - checks and displays positive and negative
- * Return: returs the positve number
- * @i: this is the parameter of the function
- */
-void positive_or_negative(int i)
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
 {
-	if (i > 0)
-		printf("%d is positive", i);
-	else if (i == 0)
-		printf("%d is zero", i);
-	else
-		printf("%d is negative", i);
+        int i;
+
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
 }
