94c3a3b66dbaa996839a4385642f43da3fad8021
Day-C <kongranalanyuy@gmail.com>
2023-03-24 10:28:35 -0700

    making

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * _isupper - chechs to find the upper case character
  * @c: c is the parameter passed into the function
  * Retuen: returns 1 if c is uppercase and 0 if not
- */
+*/
+
 int _isupper(int c)
 {
 	if  (c >= 65  && c <= 90)
