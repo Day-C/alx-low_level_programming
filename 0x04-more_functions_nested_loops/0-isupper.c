f4a6cd5ef68d27cd5bde8932fe7894b151c6b91c
Day-C <kongranalanyuy@gmail.com>
2023-03-24 12:44:10 -0700

    olive

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * _isupper - chechs to find the upper case character
- * @c: c is the parameter passed into the function
- * Retuen: returns 1 if c is uppercase and 0 if not
-*/
+ * _isupper - the function that check if parameter is caps or not
+ * @a: the parameter that ic to be ckecked
+ * Return: returns a i if uppercase and 0 if not
+ */
 
 int _isupper(int c)
 {
-	if  (c >= 65  && c <= 90)
+	if (c >=  65 && c <= 90)
 	{
-		return (1);
+		return (0);
 	}
 	return (0);
 }
