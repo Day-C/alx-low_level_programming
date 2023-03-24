6c9a0da8e20dca9680b6c4cca113fbe5cd40d7ba
Day-C <kongranalanyuy@gmail.com>
2023-03-24 12:54:12 -0700

    mentor

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,13 +2,13 @@
 
 /**
  * _isupper - the function that check if parameter is caps or not
- * @a: the parameter that ic to be ckecked
+ * @c: the parameter that ic to be ckecked
  * Return: returns a i if uppercase and 0 if not
  */
 
 int _isupper(int c)
 {
-	if (c >=  65 && c <= 90)
+	if (c >= 65 && c <= 90)
 	{
 		return (0);
 	}
