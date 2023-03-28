8d75c2e511a4ab5885e83ec8d899b9f1ca3e3195
Day-C <kongranalanyuy@gmail.com>
2023-03-28 03:03:10 -0700

    eveNumSort

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - something
+ * @str: the parameter of the function;
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		if (a % 2 == 0)
+			_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+}
