7d8315038c8b2e49d68fb9a97f50d2565e92068d
Day-C <kongranalanyuy@gmail.com>
2023-04-04 01:00:09 -0700

    search_char

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - function searches if character 'c' is in string.
+ * @s: the pointer to the string to be searched
+ * @c: the character being searched from the string
+ * Return: returns a chareacter
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+	while (i >= '\0')
+	{
+		if (s[i] == c)
+			return (s + i);
+		i++;
+	}
+	return ('\0');
+}
