ccc0c0d8088d0926e82835f829b7987fec8cfa4c
Day-C <kongranalanyuy@gmail.com>
2023-04-04 09:33:05 -0700

    locate_substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,16 +11,19 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i;
 
+	i = 0;
 	while (*haystack)
 	{
-		for (i = 0; needle[i]; i++)
+		if (haystack[i] == needle[i])
 		{
-			if (*haystack == *needle)
-			{
-				return (haystack++);
-			}
+			do {
+				if (needle[i + 1] == '\0')
+				return (haystack);
+				i++;
+			} while (haystack[i] == needle[i]);
 		}
 		haystack++;
+
 	}
 	return ('\0');
 }
