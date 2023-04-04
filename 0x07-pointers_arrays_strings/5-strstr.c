e9e31eeadfe192637a92f69ecae11060f97c521a
Day-C <kongranalanyuy@gmail.com>
2023-04-04 09:41:30 -0700

    locateAString

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,6 +11,10 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i;
 
+	if (*needle == 0)
+	{
+		return (haystack);
+	}
 	i = 0;
 	while (*haystack)
 	{
@@ -18,7 +22,7 @@ char *_strstr(char *haystack, char *needle)
 		{
 			do {
 				if (needle[i + 1] == '\0')
-				return (haystack);
+					return (haystack);
 				i++;
 			} while (haystack[i] == needle[i]);
 		}
