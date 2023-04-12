01e1ab0ca477aa12def755935d9a1667b814ae95
Day-C <kongranalanyuy@gmail.com>
2023-04-12 15:05:27 -0700

    contat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,6 +23,8 @@ char *str_concat(char *s1, char *s2)
 	while (s2[j] != '\0')
 		j++;
 	str = malloc(sizeof(char) * (i + j + 1));
+	if (str == null)
+		return (NULL);
 	i = j = 0;
 	while (s1[i] != '\0')
 	{
