842b4b7695bf50a3746c77232319830bc9030b37
Day-C <kongranalanyuy@gmail.com>
2023-04-14 16:17:18 -0700

    mem_aloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checker - function alocates memory
+ * @b: integer input
+ * Return: nothing
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *mem;
+
+	mem = malloc(b);
+
+	if (mem == NULL)
+		exit(98);
+	return (mem);
+}
