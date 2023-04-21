0c440df51d17461e0abe6a95d40c21a7eecd7f89
Day-C <kongranalanyuy@gmail.com>
2023-04-21 09:30:47 -0700

    function returs sum of all unsigned ints

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * sum_them_all - function sums all the integer inputs
+ * @n: the number of arguments
+ * Return: returns the sum or 0 if "n" is exactly 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int l, sum = 0;
+	va_list numlist;
+
+	va_start(numlist, n);
+
+	if (n == 0)
+		return (0);
+	for (l = 0; l < n; l++)
+	{
+		sum += va_arg(numlist, int);
+	}
+	va_end(numlist);
+	return (sum);
+}
