f59be31c1d71e19d0422e685f7e8f2a1b4cfa162
Day-C <kongranalanyuy@gmail.com>
2023-04-28 02:01:17 -0700

    printf linked list content

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,15 +8,16 @@
 size_t print_list(const list_t *h)
 {
 	size_t st = 0;
+
 	while (h)
 	{
-		if (!h -> str)
+		if (!h->str)
 		{
-			printf("[0] (nuil");
+			printf("[0](nil");
 		}
-		else 
-			printf("[%u] %s\n", h -> len, h -> str);
-		h = h -> next;
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
 		st++;
 	}
 	return (st);
