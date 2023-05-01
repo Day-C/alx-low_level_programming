2c603a6b6d317399108dde9b7589a38e021af8e5
Day-C <kongranalanyuy@gmail.com>
2023-05-01 15:06:57 -0700

    Number if nodes

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,1 +1,15 @@
-#include 
+#include "lists.h"
+/**
+ * listint_len - function returns the number of elements in a list.
+ * @h: linked list of type listint_t
+ * Return: return the number of nodes in the list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	for (count = 0; h; count++)
+		h = h->next;
+
+	return (count);
+}
