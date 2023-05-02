d4da30b5ded8e234e117b213c800d9be99da8890
Day-C <kongranalanyuy@gmail.com>
2023-05-01 18:33:38 -0700

    nthNODE

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,1 +1,20 @@
-#include
+#include "lists.h"
+/**
+ * get_nodeint_at_index - function returns the nth node of a liked list
+ * @head: the first element of the list
+ * @index: the node to be found
+ * Return: return NULL if node does not exist or node if it does
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *temp;
+	unsigned int num = 0;
+
+	temp = head;
+	for (num = 0; num < index;  num++)
+	{
+		if (temp)
+			temp = temp->next;
+	}
+	return (temp ? temp : NULL);
+}
