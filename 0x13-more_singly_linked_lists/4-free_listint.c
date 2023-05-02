99c0dba1a26f66829e7f320fa5edb4cb0a11e573
Day-C <kongranalanyuy@gmail.com>
2023-05-01 17:24:53 -0700

    freeLIAST

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,2 +1,18 @@
-#include
+#include "lists.h"
+/**
+ * free_listint - function frees the liked list
+ * @head: the begining of the list
+ * Return: nothing (void)
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
 
