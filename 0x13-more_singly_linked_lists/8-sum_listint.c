c96ea8516d0bdbf95e2addd0c7fb12fb201b60cf
Day-C <kongranalanyuy@gmail.com>
2023-05-01 19:01:19 -0700

    addingALL_List content

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,1 +1,19 @@
-#include
+#include "lists.h"
+/**
+ * sum_listint - function sums all data of a list.
+ * @head: the first element of the list
+ * Retutn: returns sum or 0 if list is empty
+ */
+int sum_listint(listint_t *head)
+{
+	listint_t *temp;
+	int i;
+
+	temp = head;
+	while (temp)
+	{
+		i += temp->n;
+		temp = temp->next;
+	}
+	return (i);
+}
