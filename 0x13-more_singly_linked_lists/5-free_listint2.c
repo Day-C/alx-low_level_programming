d73e76ea98d221a96820dfa57764c99937572db6
Day-C <kongranalanyuy@gmail.com>
2023-05-01 17:40:12 -0700

    freeing

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,1 +1,18 @@
-#include
+#include "lists.h"
+/**
+ * free_listint2 - function frees a linked list
+ * @head: pointer to first element on list
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	*head = NULL;
+}
