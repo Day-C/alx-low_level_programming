c99204ec613a20bd897c4286f2f766cd4dab8c57
Day-C <kongranalanyuy@gmail.com>
2023-06-19 08:37:19 -0700

    function checks and prints content of the structure

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+/**
+ * print_dog - function prints content of a structure
+ * @d: pointer to the stucture
+ * Return: nothing (void)
+ */
+void print_dog(struct dog *d)
+{
+
+	if (!d)
+		return;
+	if (d->name == NULL)
+		printf("(nil)");
+	if (!d->age)
+		printf("(nil");
+	if (d->owner == NULL)
+		printf("(nil)");
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
