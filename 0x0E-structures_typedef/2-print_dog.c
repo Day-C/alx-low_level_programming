8631501485bf4708403a41582fc8ff57642b4200
Day-C <kongranalanyuy@gmail.com>
2023-06-19 14:44:13 -0700

    free memory block

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,17 +9,17 @@ void print_dog(struct dog *d)
 
 	if (d == NULL)
 		return;
-	if (!d->name || d->name == NULL)
+	if (d->name == NULL)
 		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
 
-	if (!d->age || d->age <  1.0)
+	if (d->age <  1.0)
 		printf("Age: (nil)\n");
 	else
 		printf("Age: %f\n", d->age);
 
-	if (!d->owner || d->owner == NULL)
+	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
