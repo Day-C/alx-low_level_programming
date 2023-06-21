f7ffebaff9afec91b0b665af61bf2908057dbf77
Day-C <kongranalanyuy@gmail.com>
2023-06-20 18:28:05 -0700

    crate structure and stroe its member values in memory

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,8 @@
 #include "dog.h"
+
+char *_cpy(char *source, char *destin);
+int _str_len(char *str);
+
 /**
  * *new_dog - function creates structure
  * @name: first member
@@ -9,7 +13,8 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr;
-
+	char *ptr_name;
+	char *ptr_owner;
 
 	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
@@ -17,9 +22,39 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(ptr);
 		return (NULL);
 	}
-	
-	ptr->name = name;
+	/* allocate memory for name anf & fill*/
+	ptr_name = malloc(sizeof(char) +_str_len(name));
+	ptr->name = _cpy(name, ptr_name);
+
 	ptr->age = age;
-	ptr->owner = owner;
+	/* allocate memory for owner & fill*/
+
+	ptr_owner = malloc(sizeof(char) + _str_len(owner));
+	ptr->owner = _cpy(owner, ptr_owner);
 	return (ptr);
+}
+
+int _str_len(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+char *_cpy(char *source, char *destin)
+{
+	int i;
+
+	for (i = 0;  i < _str_len(source) && source[i]; i++)
+	{
+	 	destin[i] = source[i];
+
+	}
+	destin[i] = '\0';
+	return (destin);
 }
