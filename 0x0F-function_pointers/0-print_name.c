7e189e1a08f115733c079485640da398a656ffff
Day-C <kongranalanyuy@gmail.com>
2023-06-21 10:29:49 -0700

    function pointer takes in char input

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,17 +1,17 @@
 #include "function_pointers.h"
 /**
- * print_name - function 
+ * print_name - function passes pointer  to pointer function
  * @name: string to print
- *
+ * @f: function pointer
  * Return: nothing (void)
  */
- void print_name(char *name, void (*f)(char *))
+void print_name(char *name, void (*f)(char *))
 {
 
 	void (*ptr)(char *);
 
 	if (name == NULL)
-			return;
+		return;
 	ptr = *f;
 	ptr(name);
 
