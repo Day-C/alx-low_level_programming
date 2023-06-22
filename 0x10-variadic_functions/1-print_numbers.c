e5c442eccb263f2b40864d1576851fba8a1be95f
Day-C <kongranalanyuy@gmail.com>
2023-06-22 13:37:17 -0700

    function prints parameter followed by a aeparator

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function prints number followed by a separator
+ * @separator: character to separate the numbers
+ * @n: number of parameters
+ * Return: nothing (void)
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list numbs;
+
+	va_start(numbs, n);
+	if (separator == NULL || n == 0)
+		return;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(numbs, int));
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+}
