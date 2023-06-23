d604026d0381cebd1d5f94f341d1900d2dcd940a
Day-C <kongranalanyuy@gmail.com>
2023-06-23 11:35:13 -0700

    function loops through an array of numbers in search of a specific number

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,33 @@
+#include "function_pointers.h"
+/**
+ * int_index - function searches for an int f_p
+ * @array: array of numbers
+ * @size: size of array
+ * @cmp: compare  function (function_pointer)
+ * Return: returns index of the first element for which cmp function
+ * does not return (0) if element maches return 1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int (*ptr)(int);
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	ptr = *cmp;
+	i = 0;
+	while (i < size)
+	{
+		ptr(array[i]);
+		if (ptr(array[i]) != 0)
+		{
+			return (i);
+			continue;
+		}
+		else
+			i++;
+	}
+	return (-1);
+}
+
+
