e2533f4367c6293b0523b621b92c3c2246b19de6
Day-C <kongranalanyuy@gmail.com>
2023-06-30 11:45:38 -0700

    function counts the number of elements in the list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * list_len - function counts the number of elements in a liked list
+ * @h: firs element of the liked list (head)
+ * Return: lenght of list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *temp;
+
+	if (h == NULL)
+		return (-1);
+	temp = h;
+	while (temp != NULL)
+	{
+		count++;
+		temp = temp->next;
+	}
+	return (count);
+}
