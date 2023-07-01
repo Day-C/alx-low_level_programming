6a82b14571a3a74507d7d7d13abd376ab584fdc9
Day-C <kongranalanyuy@gmail.com>
2023-07-01 16:16:29 -0700

    freeing a linked list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,7 +6,13 @@
  */
 void free_list(list_t *head)
 {
-	if (head == NULL)
-		return;
-	free(head);
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
 }
