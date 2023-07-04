8015d2611fe656c427dd94a9ac08f4ab6b642d1e
Day-C <kongranalanyuy@gmail.com>
2023-07-04 13:27:02 -0700

    freeing memory allocated for a liked list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint2 - function frees memory allocated to a linked list
+ * @head: double pointer to the first node of the list
+ * Return: nothing (void)
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp = (*head);
+
+	if (*head == NULL)
+		return;
+
+	while (temp != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+}
