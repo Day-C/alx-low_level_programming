d272a0c349eb604838f17d8197e93b6bf31870e6
Day-C <kongranalanyuy@gmail.com>
2023-07-04 15:22:01 -0700

    function adds a new nod at an index ti the linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,6 +18,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->n = n;
 	new->next = NULL;
 
+	if (new == NULL)
+		return (NULL);
+
 	if (idx == 0)
 	{
 		new->next = *head;
