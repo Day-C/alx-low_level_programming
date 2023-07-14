5a25466619d79577cde15ca8e1e4a30bf4157ed7
Day-C <kongranalanyuy@gmail.com>
2023-07-13 17:18:55 -0700

    function create a new file and populates it with text

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,12 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC);
 	if (fd == -1)
 		return (-1);
-	i = strlen(text_content);
+
+	i = 0;
+	while (text_content[i])
+	{
+		i++;
+	};
 
 	w = write(fd, text_content, i);
 	if (w == -1)
