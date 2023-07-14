ce31a42558d3f2cadbe196beea4aec8ffe13aaf3
Day-C <kongranalanyuy@gmail.com>
2023-07-13 17:41:36 -0700

    function create a new file and populates it with text

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,10 +10,14 @@ int create_file(const char *filename, char *text_content)
 	int i;
 	int fd;
 	int w;
+	char *fl;
 
+	fl = "fails";
 	if (filename == NULL || text_content == NULL)
+	{
+		write(1, &fl, 6);
 		return (-1);
-
+	}
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC);
 	if (fd == -1)
 		return (-1);
