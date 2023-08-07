6a2d77c9d5a42abd3661d64a45a44c67632e4bb9
Day_C <kongran alanyuy@gmail.com>
2023-08-07 16:04:50 -0700

    function crates a  file if not existent and inputs a text into it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,14 +15,16 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	while (text_content[count])
+	if (text_content)
 	{
-		count++;
+		while (text_content[count])
+		{
+			count++;
+		}
+		w = write(fd, text_content, count);
+		if (w == -1)
+			return (-1);
 	}
-	w = write(fd, text_content, count);
-	if (w == -1)
-		return (-1);
-
 	close(fd);
 	return (1);
 }
