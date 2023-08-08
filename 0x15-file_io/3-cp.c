49a53e6939a2452c9395e990745a78a174b3622b
Day_C <kongran alanyuy@gmail.com>
2023-08-08 05:55:07 -0700

    fucnction copies content formone file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,18 +1,4 @@
 #include "main.h"
-/* lenght = function checks the lenght of a string
- * @str: pointer to string
- * Return: strlenght
- */
-int lenght(char *str)
-{
-	int  len = 0;
-
-	while (str[len])
-	{
-		len++;
-	}
-	return (len);
-}
 /**
  * cp - function copies content from one file to another
  * @file_from: source file where content will be coped from
@@ -21,39 +7,57 @@ int lenght(char *str)
  */
 int cp(char *file_from, char *file_to)
 {
-	int fd1, fd2;
-       	
+	int cl1, cl2, fd1, fd2, w, r = 0;
 	char buf[1024];
 
 	fd1 = open(file_from, O_RDONLY);
 	fd2 = open(file_to, O_CREAT | O_WRONLY);
 
-
-	read(fd1, buf, 1024);
-
-	write(fd2, buf, 1024);
-
-	close(fd1);
-	close(fd2);
+	r = read(fd1, buf, 1024);
+	if (r == -1)
+		return (-1);
+	w  = write(fd2, buf, r);
+	if (w == -1)
+		return (-2);
+	cl1 = close(fd1);
+	cl2 = close(fd2);
+	if (cl1 == -1 || cl2 == -1)
+		return (-3);
 	return (1);
 }
 
 /**
  * main - fexecutable function
- * @argc: arcument count
- * @argv: argument vector
- * Return: 0 if successful and 1otherwise 
- */ 
+ * @arc: arcument count
+ * @arv: argument vector
+ * Return: 0 if successful and 1otherwise
+ */
 int main(int arc, char **arv)
 {
 	int res;
 
 	if (arc != 3)
 	{
-		dprintf(2, "Usage: %s file1 and file2", arv[0]);
-		exit(1);
+		dprintf(2, "Usage: cp file_form file_to");
+		exit(97);
 	}
 	res = cp(arv[1], arv[2]);
+
+	if (res == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", arv[1]);
+		exit(98);
+	}
+	if (res == -2)
+	{
+		dprintf(2, "Error: Can't write to %s\n", arv[2]);
+		exit(99);
+	}
+	if (res == -3)
+	{
+		dprintf(2, "Error: Can't close fd file");
+		exit(100);
+	}
 	printf("-> %i\n", res);
 	return (0);
 }
