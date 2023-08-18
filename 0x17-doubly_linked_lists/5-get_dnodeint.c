92091f0b7452d6b7b64cdb5b02e74f062b8e5ad7
Day_C <kongran alanyuy@gmail.com>
2023-08-17 20:11:24 -0700

    program prints the value of a node at a given index

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -10,7 +10,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int i = 0;
 	dlistint_t *cur;
 
-	if (index < 0)
+	if (!head)
 		return (NULL);
 	while (head)
 	{
