4f8f81b88584541cddb84358d265feeacd84efdd
Day-C <kongranalanyuy@gmail.com>
2023-11-23 13:54:59 -0800

    checking for the endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * get_endianness - function check for endianness
+ * endianess refers to the byte order in which a multi-byte value is stored.
+ * there are 3 main types (big-endian, middle_endian, and little-endian)
+ * Return: return 0 on success or 1 otherwise
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *) & i;
+	return (*c);
+}
