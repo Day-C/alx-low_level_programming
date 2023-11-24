ce791635fe2d1d3dd60d7c1cb4de5af897bed573
Day-C <kongranalanyuy@gmail.com>
2023-11-24 08:10:24 -0800

    convert from binary to decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,7 +3,7 @@
 
 /**
  * binary_to_uint - function converts a binary number to an unsigned int
- * b: pointer to the string containing the binary number
+ * @b: pointer to the string containing the binary number
  * Return: return 0 if successful or nothing therwise
  */
 unsigned int binary_to_uint(const char *b)
@@ -11,13 +11,13 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int i, num;
 
 	if (!b)
-		return(0);
+		return (0);
 	num = 0;
 	i = 0;
 	while (b[i])
 	{
 		if (b[i] < '0' || b[i] > '1')
-			return(0);
+			return (0);
 
 		num = 2 * num + (b[i] - '0');
 		i++;
