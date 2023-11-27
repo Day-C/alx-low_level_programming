eba78f1a570bb40ac81fdc98f650f4c70983f94e
Day-C <kongranalanyuy@gmail.com>
2023-11-27 10:53:54 -0800

    create file if not

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+/**
+ * create_file - function creates a file if not already exist 
+ * @filename: file
+ * @text_content: string to write in file
+ * Return: return 1 on success otherwise -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_RDONLY | O_WRONLY);
+	if (fd == -1)
+		return (-1);
+
+	w = write(fd, text_content, strlen(text_content));
+	if (w == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
