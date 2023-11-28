3e6e044adc26b1e76480e8f0c4c2f669a2bea74b
Day-C <kongranalanyuy@gmail.com>
2023-11-28 01:38:16 -0800

    read and writ

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,13 +9,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t r, w;
-	char buf[1024];
+	char *buf;
 
+	
 	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
+	buf = malloc(sizeof(char) * letters);
 	r = read(fd, buf, letters);
 	if (r == -1)
 		return (0);
