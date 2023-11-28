f48c3d86312fa297f3dc4d676bac4c2a1e847e9b
Day-C <kongranalanyuy@gmail.com>
2023-11-28 02:43:14 -0800

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,6 +9,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, w;
+	int count = 0;
 
 	if (!filename)
 		return (-1);
@@ -16,8 +17,14 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-
-	w = write(fd, text_content, strlen(text_content));
+	if (text_content)
+	{
+		while (text_content[count])
+		{
+			count++;
+		}
+	}
+	w = write(fd, text_content, count);
 	if (w == -1)
 		return (-1);
 	close(fd);
