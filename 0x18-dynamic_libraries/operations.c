a2a8d43729ca90d8bf208f8c3457240afcb93173
Day-C <kongranalanyuy@gmail.com>
2024-01-15 13:59:50 -0800

    dynamic lib of c functions that is called from python

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+/**
+ * add - functions adds 2 numbers
+ * @a: first number
+ * @b: second number
+ * Return: return sum of both numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - function subtracts number from another
+ * @a: first number
+ * @b: second number
+ * Return: return result of subttraction
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - function multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * Return: return result of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - function for division
+ * @a: first number
+ * @b: second number
+ * Return: return result of division
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - function for modulos
+ * @a: first number
+ * @b: srcond number
+ * Return: return modulos of 2 numbers
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
