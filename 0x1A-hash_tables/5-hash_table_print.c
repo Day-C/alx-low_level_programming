a31f463d193f37dd130b1a1ca594577fb01f8199
Dayc <kongranalanyuy@gmail.com>
2024-02-04 03:30:31 -0800

    print content of hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,19 +7,19 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
-	int first = 0;
+
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i] == NULL)
 			continue;
 		if (i == 0)
+		{
 			printf("{");
-		
-		printf("'%s':  '%s'",ht->array[i]->key, ht->array[i]->value);
-		first++;
-		if (first > 0)
-			printf(", '%s':  '%s'",ht->array[i]->key, ht->array[i]->value);
+			printf("'%s':  '%s'", ht->array[i]->key, ht->array[i]->value);
+			continue;
+		}
+		printf(", '%s':  '%s'", ht->array[i]->key, ht->array[i]->value);
 	}
 	printf("}\n");
 }
