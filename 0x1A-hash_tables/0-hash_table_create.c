e4eea70370072e4aa53bb88f441931406216e313
Dayc <kongranalanyuy@gmail.com>
2024-02-07 03:43:44 -0800

    create hashtable

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,6 @@
 #include "hash_tables.h"
-/** hash_table_create - function creates a hash table 
+/**
+ * hash_table_create - function creates a hash table
  * @size: array size Return:
  * Return: pointer to ash table
  */
@@ -10,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table = malloc(sizeof(hash_table_t));
 	if (!table)
-		return(NULL);
+		return (NULL);
 
 	table->size = size;
 	table->array = calloc(table->size, sizeof(hash_node_t));
