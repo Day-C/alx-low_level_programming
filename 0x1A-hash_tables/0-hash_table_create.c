542cadae2677b92abb9ec1ef298e218259891859
Dayc <kongranalanyuy@gmail.com>
2024-02-07 03:26:46 -0800

    create hashtable

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,7 +8,8 @@ hash_table_t *hash_table_create(unsigned long int size) { unsigned long int i;
 	table = malloc(sizeof(hash_table_t));
 
 	table->size = size; table->array = calloc(size, sizeof(hash_node_t));
-	for (i = 0; i < table->size; i++) table->array[i] = NULL;
+	for (i = 0; i < table->size; i++)
+		table->array[i] = NULL;
 
 	return (table);
 }
